5f50c9ef31673fd534245b4030b0e675237008f6
Arthur <arthur.lucas@edu.devinci.fr>
2023-02-01 16:15:18 +0100

    API fix

--- src/Api.js
+++ src/Api.js
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
-function Api() {
+const Api  = () => {
+  const [data, setData] = useState(null);
 
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=160c6021f73643b18ba7e0ad4b916643');
-          const json = await response.json();
-          setData(json);
-          console.log(json)
-        };
-        fetchData();
-        console.log(data, 'test')
-      }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=160c6021f73643b18ba7e0ad4b916643');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchData();
+  }, []);
+
+
 
   return (
-    <div className="App">
-      <p>{data.results[0].title}</p>
-        <img src={data.results[0].image}></img>
+    
+    <div>
+      {/* {data && <div>{JSON.stringify(data)}</div>} */}
     </div>
-  )
+  );
 }
 
 export default Api;
+
