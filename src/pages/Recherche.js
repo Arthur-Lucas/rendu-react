3991e94ae466414d1f7065898c36d80de28c3f9c
Arthur <arthur.lucas@edu.devinci.fr>
2023-02-03 17:02:40 +0100

    Arthur

--- src/pages/Recherche.js
+++ src/pages/Recherche.js
@@ -8,7 +8,10 @@ export default function Navbar(){
 
     const [data, setData] = useState(null);
 
-    const [filter, setFilter] = useState('complexSearch')
+    const [filter, setFilter] = useState('complexSearch');
+
+    const [minCarbs, setMinCarbs] = useState('1');
+    const [maxCarbs, setMaxCarbs] = useState('');
 
     const [inputText, setInputText] = useState('');
 
@@ -23,34 +26,81 @@ export default function Navbar(){
         
     }
 
-    useEffect(() => {
-        console.log(filter);
-        const fetchData = async () => {
-            var response = null
-        if(filter === 'complexSearch'){
-            console.log(filter)
-            response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=160c6021f73643b18ba7e0ad4b916643&query=' + inputText);
-        }
-        else if(filter === 'findByNutrients'){
-            response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=160c6021f73643b18ba7e0ad4b916643&minCarbs=10');
-        }
-        else if(filter === 'findByIngredients'){
-            response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=160c6021f73643b18ba7e0ad4b916643&ingredients=apples');
-        }
-        const json = await response.json();
-        setData(json);
-        console.log(data && data.results)
-        }; 
-        fetchData(); 
-    }, [inputText, filter]);
+    const handleChangeminCarbs = (e) => {
+        e.preventDefault();
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            if(maxCarbs == '' || e.target.value <= maxCarbs){
+                setMinCarbs(e.target.value);
+            } else {
+                setMinCarbs('1')
+            }
+           
+        }, 1500);
+    }
+
+    const handleChangemaxCarbs = (e) => {
+        e.preventDefault();
+        clearTimeout(timeout)
+        console.log(minCarbs)
+        timeout = setTimeout(() => {
+            if(e.target.value >= minCarbs){
+                
+        console.log(minCarbs + '2')
+                setMaxCarbs(e.target.value);
+
+            } else {
+                setMaxCarbs('')
+            }
+           
+        }, 1500);
+    }
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         var response = null
+    //     if(filter === 'complexSearch'){
+    //         console.log(filter)
+    //         response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=9185b4dc4ec64b1bbd9055313ecf227c&query=' + inputText);
+    //     }
+    //     else if(filter === 'findByNutrients'){
+    //         response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=9185b4dc4ec64b1bbd9055313ecf227c&minCarbs=' + minCarbs + (maxCarbs != '' ? '&maxCarbs=' + maxCarbs : ''));
+    //     }
+    //     else if(filter === 'findByIngredients'){
+    //         response = await fetch('https://api.spoonacular.com/recipes/' + filter + '?apiKey=9185b4dc4ec64b1bbd9055313ecf227c&ingredients=apples');
+    //     }
+    //     const json = await response.json();
+    //     setData(json);
+    //     }; 
+    //     fetchData(); 
+    // }, [inputText, filter, minCarbs, maxCarbs]);
     
     return (
         <div className='flex  flex-col items-center w-1/1'>  
         <div className='flex flex-row w-full items-center justify-center'>
             {/* textboxrecherche */}
-            <div className='w-1/4'>
-                <input className='w-full border-2 border-gray-300 rounded-lg p-2' onChange={handleChange} placeholder='Recherche'></input>
-            </div>
+            {filter === 'complexSearch' ? (
+                <div className='w-1/4'>
+                    <input className='w-full border-2 border-gray-300 rounded-lg p-2' onChange={handleChange} placeholder='Recherche'></input>
+                </div>
+            )
+            :
+            (<>
+                {filter === 'findByNutrients' ? (
+                    <div className='w-1/4'>
+                        <input type="number"  className='w-full border-2 border-gray-300 rounded-lg p-2' onChange={handleChangeminCarbs} min='1' max={maxCarbs != '' ? maxCarbs : null} placeholder='minCarbs'></input>
+                        <input type="number" className='w-full border-2 border-gray-300 rounded-lg p-2' onChange={handleChangemaxCarbs} min={minCarbs != '' ? minCarbs : null} placeholder='maxCarbs'></input>
+                    </div>
+                ) 
+                : 
+                (
+                    <div className='w-1/4'>
+                    <input className='w-full border-2 border-gray-300 rounded-lg p-2' onChange={handleChange} placeholder='Recherche'></input>
+                </div>
+                )}
+            </>)
+            }
+            
             <div className='w-1/4'>
                 <select placeholder='Catégorie' onChange={(event) => setFilter(event.target.value)} className=' border-2 border-gray-300 rounded-lg p-2'>
                     <option value='complexSearch'>Find by recipes</option>
